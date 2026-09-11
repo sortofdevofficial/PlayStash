9329fd31da11d4bfa8c060b587b9a0fc4760c7fc
sortofdevofficial <sortofdevofficial@gmail.com>
2026-09-11 13:23:31 +0530

    e

--- game/worldforge/js/models/market.js
+++ game/worldforge/js/models/market.js
@@ -1,81 +1,105 @@
 export function createMarket(id, scene) {
   const root = new BABYLON.TransformNode(id, scene);
 
+  // Materials
   const woodMat = new BABYLON.StandardMaterial(id + "_wMat", scene);
-  woodMat.diffuseColor = new BABYLON.Color3(0.42, 0.27, 0.14);
+  woodMat.diffuseColor = new BABYLON.Color3(0.4, 0.24, 0.11);
   woodMat.specularColor = new BABYLON.Color3(0, 0, 0);
   woodMat.flatShaded = true;
 
-  const clothMat = new BABYLON.StandardMaterial(id + "_clothMat", scene);
-  clothMat.diffuseColor = new BABYLON.Color3(0.75, 0.3, 0.25);
-  clothMat.specularColor = new BABYLON.Color3(0, 0, 0);
+  const clothMat = new BABYLON.StandardMaterial(id + "_cMat", scene);
+  clothMat.diffuseColor = new BABYLON.Color3(0.85, 0.25, 0.2);
+  clothMat.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
   clothMat.flatShaded = true;
 
-  const tableMat = new BABYLON.StandardMaterial(id + "_tMat", scene);
-  tableMat.diffuseColor = new BABYLON.Color3(0.48, 0.32, 0.17);
-  tableMat.specularColor = new BABYLON.Color3(0, 0, 0);
-  tableMat.flatShaded = true;
+  const clothWhiteMat = new BABYLON.StandardMaterial(id + "_cwMat", scene);
+  clothWhiteMat.diffuseColor = new BABYLON.Color3(0.9, 0.88, 0.82);
+  clothWhiteMat.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+  clothWhiteMat.flatShaded = true;
 
-  const anvilMat = new BABYLON.StandardMaterial(id + "_aMat", scene);
-  anvilMat.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.22);
-  anvilMat.specularColor = new BABYLON.Color3(0.15, 0.15, 0.15);
-  anvilMat.flatShaded = true;
+  const goldMat = new BABYLON.StandardMaterial(id + "_gMat", scene);
+  goldMat.diffuseColor = new BABYLON.Color3(0.95, 0.75, 0.15);
+  goldMat.emissiveColor = new BABYLON.Color3(0.2, 0.15, 0.02);
+  goldMat.specularColor = new BABYLON.Color3(0.5, 0.5, 0.2);
 
-  const toolMat = new BABYLON.StandardMaterial(id + "_toolMat", scene);
-  toolMat.diffuseColor = new BABYLON.Color3(0.6, 0.62, 0.65);
-  toolMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
-  toolMat.flatShaded = true;
+  const sackMat = new BABYLON.StandardMaterial(id + "_sMat", scene);
+  sackMat.diffuseColor = new BABYLON.Color3(0.65, 0.52, 0.35);
+  sackMat.specularColor = new BABYLON.Color3(0, 0, 0);
 
-  // Market stall frame
-  const postPositions = [[-0.8, -0.8], [0.8, -0.8], [-0.8, 0.8], [0.8, 0.8]];
-  postPositions.forEach(([px, pz], i) => {
-    const post = BABYLON.MeshBuilder.CreateCylinder(id + "_post_" + i, { height: 1.5, diameter: 0.1, tessellation: 6 }, scene);
-    post.position.set(px, 0.75, pz);
+  const fruitMat = new BABYLON.StandardMaterial(id + "_fMat", scene);
+  fruitMat.diffuseColor = new BABYLON.Color3(0.8, 0.15, 0.1);
+
+  // Market Counter / Floor Platform
+  const platform = BABYLON.MeshBuilder.CreateBox(id + "_plat", { width: 1.8, height: 0.1, depth: 1.4 }, scene);
+  platform.position.y = 0.05;
+  platform.material = woodMat;
+  platform.parent = root;
+
+  // Counter
+  const counter = BABYLON.MeshBuilder.CreateBox(id + "_counter", { width: 1.6, height: 0.45, depth: 0.4 }, scene);
+  counter.position.set(0, 0.275, -0.3);
+  counter.material = woodMat;
+  counter.parent = root;
+
+  // Canopy Posts (4 corners)
+  const posts = [
+    [-0.8, -0.6], [0.8, -0.6],
+    [-0.8, 0.6],  [0.8, 0.6]
+  ];
+  posts.forEach(([px, pz], i) => {
+    const post = BABYLON.MeshBuilder.CreateCylinder(id + "_post_" + i, { height: 1.2, diameter: 0.07, tessellation: 6 }, scene);
+    post.position.set(px, 0.6, pz);
     post.material = woodMat;
     post.parent = root;
   });
 
-  // Awning/canopy
-  const awning = BABYLON.MeshBuilder.CreateCylinder(id + "_awning", { height: 0.5, diameterTop: 0.1, diameterBottom: 2.1, tessellation: 4 }, scene);
-  awning.position.y = 1.65;
-  awning.rotation.y = Math.PI / 4;
-  awning.material = clothMat;
-  awning.parent = root;
+  // Striped Canopy Roof
+  const canopySegments = 6;
+  const segWidth = 1.8 / canopySegments;
+  for (let i = 0; i < canopySegments; i++) {
+    const stripe = BABYLON.MeshBuilder.CreateBox(id + "_roof_" + i, { width: segWidth, height: 0.04, depth: 1.5 }, scene);
+    stripe.position.set(-0.9 + segWidth * (i + 0.5), 1.25 + Math.sin((i / canopySegments) * Math.PI) * 0.1, 0);
+    stripe.rotation.z = (i - canopySegments / 2 + 0.5) * -0.03;
+    stripe.material = i % 2 === 0 ? clothMat : clothWhiteMat;
+    stripe.parent = root;
+  }
 
-  // Trading table
-  const table = BABYLON.MeshBuilder.CreateBox(id + "_table", { width: 1.5, height: 0.12, depth: 0.7 }, scene);
-  table.position.set(0, 0.55, 0.4);
-  table.material = tableMat;
-  table.parent = root;
+  // Display Goods on Counter: Sacks & Fruit
+  const sack1 = BABYLON.MeshBuilder.CreateSphere(id + "_sack1", { diameterX: 0.25, diameterY: 0.3, diameterZ: 0.25, segments: 6 }, scene);
+  sack1.position.set(-0.5, 0.6, -0.3);
+  sack1.material = sackMat;
+  sack1.parent = root;
 
-  const tableLegPositions = [[-0.65, 0.35], [0.65, 0.35], [-0.65, 0.5], [0.65, 0.5]];
-  tableLegPositions.forEach(([px, pz], i) => {
-    const leg = BABYLON.MeshBuilder.CreateBox(id + "_tleg_" + i, { width: 0.08, height: 0.5, depth: 0.08 }, scene);
-    leg.position.set(px, 0.28, pz);
-    leg.material = woodMat;
-    leg.parent = root;
-  });
+  const sack2 = BABYLON.MeshBuilder.CreateSphere(id + "_sack2", { diameterX: 0.2, diameterY: 0.25, diameterZ: 0.2, segments: 6 }, scene);
+  sack2.position.set(-0.3, 0.58, -0.3);
+  sack2.material = sackMat;
+  sack2.parent = root;
 
-  // Anvil for tool-crafting flavor
-  const anvilBase = BABYLON.MeshBuilder.CreateCylinder(id + "_anvilBase", { height: 0.25, diameter: 0.25, tessellation: 6 }, scene);
-  anvilBase.position.set(-0.4, 0.15, -0.35);
-  anvilBase.material = anvilMat;
-  anvilBase.parent = root;
+  // Gold coins stack / box on counter
+  const goldBox = BABYLON.MeshBuilder.CreateBox(id + "_goldBox", { width: 0.2, height: 0.12, depth: 0.2 }, scene);
+  goldBox.position.set(0.4, 0.55, -0.3);
+  goldBox.material = goldMat;
+  goldBox.parent = root;
 
-  const anvilTop = BABYLON.MeshBuilder.CreateBox(id + "_anvilTop", { width: 0.4, height: 0.12, depth: 0.16 }, scene);
-  anvilTop.position.set(-0.4, 0.32, -0.35);
-  anvilTop.material = anvilMat;
-  anvilTop.parent = root;
+  // Fruit crates / apples
+  for (let f = 0; f < 4; f++) {
+    const apple = BABYLON.MeshBuilder.CreateSphere(id + "_apple_" + f, { diameter: 0.09, segments: 5 }, scene);
+    apple.position.set(0.0 + (f % 2) * 0.1, 0.53 + Math.floor(f / 2) * 0.08, -0.3 + (f % 2) * 0.05);
+    apple.material = fruitMat;
+    apple.parent = root;
+  }
 
-  // Finished tools laid out on the table
-  const toolPositions = [[-0.3, 0.63, 0.35], [0.0, 0.63, 0.45], [0.3, 0.63, 0.35]];
-  toolPositions.forEach(([px, py, pz], i) => {
-    const tool = BABYLON.MeshBuilder.CreateCylinder(id + "_tool_" + i, { height: 0.28, diameter: 0.045, tessellation: 5 }, scene);
-    tool.rotation.z = Math.PI / 2.4;
-    tool.position.set(px, py, pz);
-    tool.material = toolMat;
-    tool.parent = root;
-  });
+  // Market Sign on front beam
+  const signBeam = BABYLON.MeshBuilder.CreateBox(id + "_signBeam", { width: 0.6, height: 0.2, depth: 0.03 }, scene);
+  signBeam.position.set(0, 1.1, -0.65);
+  signBeam.material = woodMat;
+  signBeam.parent = root;
+
+  const signCoin = BABYLON.MeshBuilder.CreateCylinder(id + "_signCoin", { diameter: 0.14, height: 0.02, tessellation: 10 }, scene);
+  signCoin.rotation.x = Math.PI / 2;
+  signCoin.position.set(0, 1.1, -0.67);
+  signCoin.material = goldMat;
+  signCoin.parent = root;
 
   return root;
 }
